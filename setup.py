ccd45f410cd8db809143772b4bb3da9556935132
Bryce Willey <bryce.willey@suffolk.edu>
2025-11-24 16:59:00 -0500

    Use implicit namespaces over explicit namespaces

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
 
@@ -51,8 +51,7 @@ setup(name='docassemble.AppealsCivilDocketingStatement',
       author_email='bwilley@suffolk.edu',
       license='The MIT License (MIT)',
       url='https://courtformsonline.org',
-      packages=find_packages(),
-      namespace_packages=['docassemble'],
+      packages=find_namespace_packages(),
       install_requires=['docassemble.AssemblyLine>=2.23.0', 'docassemble.MassAccess>=0.0.3.1', 'docassemble.MassAppealsCourt'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/AppealsCivilDocketingStatement/', package='docassemble.AppealsCivilDocketingStatement'),
